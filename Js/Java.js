c17e57712b71d2dc9cb1ec9a47308b3388c3df74
Ali Kotb <177870702+alimohamedstem@users.noreply.github.com>
2025-07-19 01:50:55 +0300

    add feedback

--- Js/Java.js
+++ Js/Java.js
@@ -279,3 +279,34 @@ async function renderFiles() {
 }
 
 renderFiles();
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollers = document.querySelectorAll(".scroller");
+
+    // If a user hasn't opted in for reduced motion, then we add the animation
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        addAnimation();
+    }
+
+    function addAnimation() {
+        scrollers.forEach((scroller) => {
+            // add data-animated="true" to every `.scroller` on the page
+            scroller.setAttribute("data-animated", true);
+
+            // Make an array from the elements within `.scroller-inner`
+            const scrollerInner = scroller.querySelector(".scroller__inner");
+            const scrollerContent = Array.from(scrollerInner.children);
+
+            // For each item in the array, clone it
+            // add aria-hidden to it
+            // and add it back to the `.scroller-inner`
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute("aria-hidden", true);
+                scrollerInner.appendChild(duplicatedItem);
+            });
+        });
+    }
+});
